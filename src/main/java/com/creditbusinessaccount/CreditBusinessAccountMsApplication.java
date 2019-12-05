47dabd2924b2143ef8b15b50c45a0077cb457162
U-USERSAD\ptrinida <ptrinida@LIM-JTZXMF2.usersad.everis.int>
2019-12-04 23:09:16 -0500

    Seventh Microservice

---
+++ src/main/java/com/creditbusinessaccount/CreditBusinessAccountMsApplication.java
@@ -0,0 +1,13 @@
+package com.creditbusinessaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditBusinessAccountMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditBusinessAccountMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creditbusinessaccount/CreditBusinessAccountMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditbusinessaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditBusinessAccountMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
